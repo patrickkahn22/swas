9e528bc9554270cd40d7004db899e302dca25e9b
patrickkahn22 <patrickkuhn@protonmail.com>
2017-05-04 11:08:40 +0200

    Update example-1.2.x.js

--- example-1.2.x.js
+++ example-1.2.x.js
@@ -1,13 +1,13 @@
 // Download the Node helper library from twilio.com/docs/node/install
 // These vars are your accountSid and authToken from twilio.com/user/account
-var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-var authToken = "your_auth_token";
+var accountSid = 'AC1f9aa28b31298ccc6544e06c8ae1e457';
+var authToken = "ac70e1878e4e7bf86bd6703d2c65c975";
 var client = require('twilio')(accountSid, authToken);
 
 client.calls.create({
     url: "http://demo.twilio.com/docs/voice.xml",
-    to: "+14155551212",
-    from: "+15017250604"
+    to: "+17202047287",
+    from: "+4915735983489"
 }, function(err, call) {
     process.stdout.write(call.sid);
 });
